1d6784b628604dc565290c6ceb73ab601d470ab8
kdraaisma <Kaspar@Kaspars-iMac.lan>
2017-06-01 00:54:40 +0200

    Minor fixes in index.js for server URL

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ var api = new ParseServer({
   // The public URL of your app.
   // This will appear in the link that is used to verify email addresses and reset passwords.
   // Set the mount path as it is in serverURL
-  publicServerURL: process.env.SERVER_URL,
+  publicServerURL: ‘https://id-sim3.herokuapp.com/parse’,
   // Your apps name. This will appear in the subject and body of the emails that are sent.
   appName: ‘ID-SIM Survey’,
   // The email adapter
